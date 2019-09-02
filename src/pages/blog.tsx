e338b81c22f74d8cf4681fc234e589e467ed6b9e
Sebi <SebastianStoff@gmx.at>
2019-09-02 16:43:24 +0200

    blog page is now responsible for displaying the summary of all blogs

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -2,13 +2,45 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import BlogPreview from "../components/BlogPreview"
+import { graphql } from "gatsby"
 
-const NotFoundPage: React.FC = () => (
+interface Props {
+  data: any //from GraphQl
+}
+
+const NotFoundPage: React.FC<Props> = ({ data }) => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Blog" />
+    <h1>Blog</h1>
+    <p>Welcome to the Blog-Overview!</p>
+    {data.allMarkdownRemark.edges.map(post => (
+      <BlogPreview
+        title={post.node.frontmatter.title}
+        date={post.node.frontmatter.date}
+        author={post.node.frontmatter.author}
+        path={post.node.frontmatter.path}
+      ></BlogPreview>
+    ))}
   </Layout>
 )
 
 export default NotFoundPage
+
+export const IndexQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date(formatString: "DD.MM.YYYY")
+            path
+            author
+          }
+          excerpt(pruneLength: 200)
+        }
+      }
+    }
+  }
+`
