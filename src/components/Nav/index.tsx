e15bedff53e944a2818f9bbf6848d66870c4cb9a
Sebi <SebastianStoff@gmx.at>
2019-09-03 13:57:10 +0200

    added base bulma navbar

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,17 +1,59 @@
-
-import React from "react";
+import React from "react"
 import { Link } from "gatsby"
-import styles from "./styles.module.scss";
+import styles from "./styles.module.scss"
 
 const Nav: React.FC = () => {
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          {/* <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"> */}
+          Logo here
+        </a>
 
-    return (<nav className={styles.nav}>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/blog">Blog</Link></li>
-            <li><Link to="/about">About</Link></li>
-        </ul>
-    </nav>)
+        <a
+          role="button"
+          className="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <Link to="/" className="navbar-item">Home</Link>
+          <Link to="/blog" className="navbar-item">Blog</Link>
+          <Link to="/about" className="navbar-item">About</Link>
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">More</a>
+
+            <div className="navbar-dropdown">
+              <a className="navbar-item">About</a>
+              <a className="navbar-item">Jobs</a>
+              <a className="navbar-item">Contact</a>
+              <hr className="navbar-divider" />
+              <a className="navbar-item">Report an issue</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a className="button is-light">Log in</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
-export default Nav;
+export default Nav
