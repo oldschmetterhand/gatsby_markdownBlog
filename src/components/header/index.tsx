99c117f7b993554265f78e06f69f0677e242d744
Sebi <SebastianStoff@gmx.at>
2019-09-03 14:22:39 +0200

    changed to bulma header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,37 +1,21 @@
 import { Link } from "gatsby"
 import React from "react"
-import { Stream } from "stream";
+import styles from "./styles.module.scss"
 
 interface Props {
   siteTitle: string
 }
 
 const Header: React.FC<Props> = ({ siteTitle = `` }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <section className="hero is-medium is-primary is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">NAMPI</h1>
+          <h2 className="subtitle">Nuns and Monks Prosopographical Interfaces</h2>
+        </div>
+      </div>
+    </section>
   </header>
 )
 
