4dc6b544adc12891d987ecb320d52db7e97f7f0e
Sebi <SebastianStoff@gmx.at>
2019-09-03 09:55:04 +0200

    changed headings a bit

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,7 +5,7 @@ import SEO from "../components/seo"
 const SecondPage: React.FC = () => (
   <Layout>
     <SEO title="about" />
-    <h1>About Page</h1>
+    <h1>About</h1>
     <p>Short Description here --- about Project / Documentatione etc.</p>
   </Layout>
 )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import SEO from "../components/seo"
 const IndexPage: React.FC = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Welcome</h1>
+    <h1>Home</h1>
     <p>This blog template is based on Gatsby.js.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>  
