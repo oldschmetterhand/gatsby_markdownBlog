ed9819df6e82ab61702913b106e64bddeb949313
Sebi <SebastianStoff@gmx.at>
2019-09-03 11:28:59 +0200

    changed folder name + imports

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Nav from "../Nav"
-import Header from "../header"
+import Header from "../Header"
 
 const Layout: React.FC = ({ children }) => {
   const data = useStaticQuery(graphql`
