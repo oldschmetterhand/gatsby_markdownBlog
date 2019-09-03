314e339a463da1672a5ffe7cedb8ff8c2b5efbde
Sebi <SebastianStoff@gmx.at>
2019-09-03 15:40:03 +0200

    added giant nampi image at header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,6 @@
 import React from "react"
+import Image from "../image"
+import styles from "./styles.module.scss"
 
 interface Props {
   siteTitle: string
@@ -11,7 +13,12 @@ const Header: React.FC<Props> = ({ siteTitle = `` }) => (
         <div className="container">
           <h1 className="title">{siteTitle}</h1>
           <h2 className="subtitle">React/Gatsby, Bulma, </h2>
+          <br></br>
+          <div className={["container is-fluid", styles.imageHolder].join(" ")}>
+            <Image></Image>
+          </div>
         </div>
+        
       </div>
     </section>
   </header>
