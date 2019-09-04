8023e1c1897f47f14dfefa95cf8080093760bf8d
Sebi <SebastianStoff@gmx.at>
2019-09-04 14:47:41 +0200

    removed box class

--- src/components/layout/TitleGrid/index.tsx
+++ src/components/layout/TitleGrid/index.tsx
@@ -1,32 +1,25 @@
 import React from "react"
 
 interface Props {
-    rightTop?: any,
-    rightBottom?: any
+  rightTop?: any
+  rightBottom?: any
 }
 
-const TitleGrid:React.FC<Props> = ({
-    children,
-    rightTop = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proinornare magna eros",
-    rightBottom = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proinornare magna eros"
+const TitleGrid: React.FC<Props> = ({
+  children,
+  rightTop = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proinornare magna eros",
+  rightBottom = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proinornare magna eros",
 }) => {
   return (
     <div className="tile is-ancestor">
       <div className="tile is-8 is-vertical is-parent">
-        <div className="tile is-child box">
-          {children}  
-        </div>
-        
+        <div className="tile is-child">{children}</div>
       </div>
       <div className="tile is-parent is-vertical">
-        <div className="tile is-child box">
-          {rightTop}
-        </div>
-        <div className="tile is-child box">
+        <div className="tile is-child">{rightTop}</div>
+        <div className="tile is-child">
           <p className="title">Two</p>
-          <p>
-            {rightBottom}
-          </p>
+          <p>{rightBottom}</p>
         </div>
       </div>
     </div>
