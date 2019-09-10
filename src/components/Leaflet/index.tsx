f2ea2b0dc4d464ae70f5ab9cd9a7b26cb18b3557
Sebi <SebastianStoff@gmx.at>
2019-09-10 09:35:15 +0200

    working base map for leaflet

--- src/components/Leaflet/index.tsx
+++ src/components/Leaflet/index.tsx
@@ -3,32 +3,34 @@ import React, { useEffect, useState } from "react"
 interface Props {}
 
 const Leaflet: React.FC = () => {
-  const [L, setL] = useState(undefined)
-  const [map, setMap] = useState(undefined)
 
   useEffect(() => {
-    if ((window as any).L) {
-      console.log(window.L)
-      setL((window as any).L)
-      let map = L.map("map").setView([51.505, -0.09], 13);
-      setMap(map)
-      initMap()
+    console.info(`%c Run useEffect on didMount.`)
+    try {
+      let sample = window.L.map;
+      let map = window.L.map("map").setView([51.505, -0.09], 13);
+      initMap(map);
+      console.info(`%c Leaflet map succesfully initialized`, 'color:green')
+    } catch {
+      console.debug(`There was an error`);
+
     }
   }, [(window as any).L])
 
-  const initMap = () => {
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  const initMap = (map) => {
+    console.info("Running initMap()")
+    window.L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map)
 
-    L.marker([51.5, -0.09])
+    window.L.marker([51.5, -0.09])
       .addTo(map)
       .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
       .openPopup()
   }
 
-  return <div id="map" style={{ height: "180px" }}></div>
+  return <div id="map" style={{ height: "360px" }}></div>
 }
 
 export default Leaflet
