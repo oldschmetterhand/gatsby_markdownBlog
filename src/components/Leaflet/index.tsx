fc71333454bc187777e6074e92e5c08b3db46d41
Sebi <SebastianStoff@gmx.at>
2019-09-10 08:28:10 +0200

    starting own leaflet component

---
+++ src/components/Leaflet/index.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react"
+
+interface Props {
+
+}
+
+const Leaflet: React.FC = () => {
+  
+
+
+  return <div id="mapid">
+
+
+
+  </div>
+}
+
+export default Leaflet
