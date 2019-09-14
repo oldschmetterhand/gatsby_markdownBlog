456008a831cb16604f30caa9372b0ed699f0071c
Sebi <SebastianStoff@gmx.at>
2019-09-14 13:07:34 +0200

    changed layout from basse Post

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,26 +1,35 @@
 import React from "react"
 import Layout from "../Layout"
+import TitleGrid from "../layout/TitleGrid"
 import SEO from "../Seo"
+import Img from "gatsby-image"
 
 interface Props {
   title: string
   date: string
   author: string
+  previewImage?: any
 }
 
-const Post: React.FC<Props> = ({ title, date, author, children }) => {
+const Post: React.FC<Props> = ({ title, date, author, children, previewImage }) => {
+  const metaData = (
+    <div className="blog-post">
+      <h2>Article Info</h2>
+      <br></br>
+      <Img fixed={previewImage}></Img>
+      <hr></hr>
+      <h3>{title}</h3>
+      <h4>{date}</h4>
+      <h5>{author}</h5>
+    </div>
+  )
+
   return (
-    <Layout showHeader={false}>
+    <Layout>
       <SEO title={title}></SEO>
-      <div className="blog-post-container">
-        <div className="blog-post">
-          <h3>{title}</h3>
-          <h4>{date}</h4>
-          <h5>{author}</h5>
-          <hr></hr>
-          <div className="content">{children}</div>
-        </div>
-      </div>
+      <TitleGrid rightTop={metaData} rightBottom={null}>
+        <div className="content">{children}</div>
+      </TitleGrid>
     </Layout>
   )
 }
