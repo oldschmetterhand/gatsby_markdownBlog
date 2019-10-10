5276c4eee76afef4b064ed8e7f1cd1abb3944aa3
Sebi <SebastianStoff@gmx.at>
2019-10-10 18:08:10 +0200

    adjusted title + subtitle

--- src/components/specific/Header/index.tsx
+++ src/components/specific/Header/index.tsx
@@ -2,22 +2,27 @@ import React, { useState } from "react"
 import styles from "./styles.module.scss"
 
 interface Props {
-  siteTitle: string
+  siteTitle: string,
+  subTitle: string,
   showTeaser?: boolean
 }
 
 const Header: React.FC<Props> = ({ 
   siteTitle = ``,
+  subTitle = ``,
   showTeaser = false 
 }) => {
+
+  const TITLE = siteTitle;
+  const SUB_TITLE = subTitle; 
 
   const baseHeader = (
     <header className={styles.baseHeader}>
       <section className="hero has-text-centered">
         <div className="hero-body">
           <div className={["container", styles.heroEnd].join(" ")}>
-            <h1 className="title">ZIM Blog</h1>
-            <h2 className="subtitle">Based on React/Gatsby and Bulma</h2>
+            <h1 className="title">{TITLE}</h1>
+            <h2 className="subtitle">{SUB_TITLE}</h2>
           </div>
         </div>
       </section>
@@ -35,7 +40,7 @@ const Header: React.FC<Props> = ({
           <div className="container">
             <h1 className="title has-text-black">{siteTitle}</h1>
             <h2 className="subtitle has-text-white-ter">
-              React/Gatsby, Bulma,{" "}
+              {SUB_TITLE}
             </h2>
             <br></br>
             <div
