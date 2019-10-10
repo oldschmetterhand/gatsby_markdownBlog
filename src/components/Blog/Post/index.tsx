01091aa6af5838213615ffa77b628a524a2309b5
Sebi <SebastianStoff@gmx.at>
2019-10-10 16:11:13 +0200

    removed unnecessary functionality

--- src/components/Blog/Post/index.tsx
+++ src/components/Blog/Post/index.tsx
@@ -4,7 +4,6 @@ import TitleGrid from "../../layout/TitleGrid"
 import SEO from "../../Seo"
 import Img from "gatsby-image"
 import styles from "./styles.module.scss"
-import BackButton from "../../BackButton"
 
 interface Props {
   title: string
@@ -24,7 +23,6 @@ const Post: React.FC<Props> = ({ title, date, author, children, previewImage = u
       <h3>{date}</h3>
       <h3>{author}</h3>
       <br></br>
-      <BackButton/>
     </div>
   )
 
