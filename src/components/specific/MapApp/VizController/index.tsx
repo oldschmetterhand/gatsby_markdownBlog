d09015f456c87142b5133b57fa7df022f8e2120c
Sebi <SebastianStoff@gmx.at>
2019-10-11 10:01:57 +0200

    added dummy data + waiting now until leaflet is completely initialued in controller

--- src/components/specific/MapApp/VizController/index.tsx
+++ src/components/specific/MapApp/VizController/index.tsx
@@ -2,19 +2,45 @@ import React, {useState, useEffect} from "react"
 import TimeLine from "./Timeline"
 import LeafletMap from "./LeafletMap"
 import AppLayout from "./AppLayout"
+import {VizEvent } from "../index"
 
-const VizController = () => {
+const dummyData: VizEvent[] = [
+    {
+        title: "test01",
+        date: undefined,
+        lMarker: undefined,
+        primSource: "Augustus",
+        secSource: "Rostovzef"
+    },
+    {
+        title: "test02",
+        date: undefined,
+        lMarker: undefined,
+        primSource: "Augustus",
+        secSource: "Rostovzef"
+    }
+] 
+
+
+interface Props {
+    vizEvents: VizEvent[]
+}
+
+const VizController: React.FC<Props> = ({vizEvents = dummyData}) => {
 
   const [leafletInitialized, setLeafletInitialized] = useState<boolean>(false);
 
   const getLeafletStatus = (mapInitialized: boolean, dataLoaded: boolean): void => {
-    setLeafletInitialized(mapInitialized)
+    let isInitialized = mapInitialized && dataLoaded;
+    setLeafletInitialized(isInitialized)
   }
 
   useEffect( () =>{
     if(leafletInitialized){
 
-        //Start operations here
+        //Start operations here -- create Leaflet markers! // then draw markers to map?
+        
+
 
     }
   },[leafletInitialized])
