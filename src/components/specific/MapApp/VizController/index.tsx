53f06ac9bcd355aa470b67b51aafd096b8ff64d9
Sebi <SebastianStoff@gmx.at>
2019-10-11 09:47:21 +0200

    added procedure to check if leaflet is succesfully initialized

--- src/components/specific/MapApp/VizController/index.tsx
+++ src/components/specific/MapApp/VizController/index.tsx
@@ -1,14 +1,33 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import TimeLine from "./Timeline"
 import LeafletMap from "./LeafletMap"
 import AppLayout from "./AppLayout"
 
 const VizController = () => {
+
+  const [leafletInitialized, setLeafletInitialized] = useState<boolean>(false);
+
+  const getLeafletStatus = (mapInitialized: boolean, dataLoaded: boolean): void => {
+    setLeafletInitialized(mapInitialized)
+  }
+
+  useEffect( () =>{
+    if(leafletInitialized){
+
+        //Start operations
+        console.log("LEAFLET NOW INITED");
+
+    }
+  },[leafletInitialized])
+
   return (
     <>
       <AppLayout
         leftCol={<TimeLine></TimeLine>}
-        middleCol={<LeafletMap></LeafletMap>}
+        middleCol={
+        <LeafletMap
+            tellLStatus={getLeafletStatus}
+        ></LeafletMap>}
       ></AppLayout>
     </>
   )
