52599576e7d71beb22a1d16677b0b42d5a2dba5a
Sebi <SebastianStoff@gmx.at>
2019-10-11 08:55:08 +0200

    added leaflet specifics

--- src/components/specific/MapApp/index.tsx
+++ src/components/specific/MapApp/index.tsx
@@ -1,34 +1,47 @@
-
-import React from 'react';
-import VizController from './VizController'
+import React from "react"
+import VizController from "./VizController"
+import Helmet from "react-helmet"
 
 export interface LeafletMarker {
-    x: number
-    y: number
-    popUpContent: string
-    group: string
-    boundTo?: any
-    lMarkerRef?: any
-  }
+  x: number
+  y: number
+  popUpContent: string
+  group: string
+  boundTo?: any
+  lMarkerRef?: any
+}
 
 export interface VizEvent {
-    title: string,
-    date: string | undefined,
-    lMarker: LeafletMarker | undefined,
-    primSource: string,
-    secSource: string,
+  title: string
+  date: string | undefined
+  lMarker: LeafletMarker | undefined
+  primSource: string
+  secSource: string
 }
 
 interface Props {
-    vizEvents?: VizEvent[]
+  vizEvents?: VizEvent[]
 }
 
-
-const MapApp: React.FC<Props> = ({vizEvents = undefined}) => {
-    return <>
-        <VizController></VizController>
+const MapApp: React.FC<Props> = ({ vizEvents = undefined }) => {
+  return (
+    <>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
+          integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+          crossorigin=""
+        />
+        <script
+          src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
+          integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
+          crossorigin=""
+        ></script>
+      </Helmet>
+      <VizController></VizController>
     </>
+  )
 }
 
-export default MapApp;
-
+export default MapApp
