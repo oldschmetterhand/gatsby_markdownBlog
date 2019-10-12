267ae8be82d57949d9705d4465644490286d7aff
Sebi <SebastianStoff@gmx.at>
2019-10-12 09:34:41 +0200

    changed style

--- src/components/specific/MapApp/VizController/Summary/index.tsx
+++ src/components/specific/MapApp/VizController/Summary/index.tsx
@@ -15,13 +15,19 @@ const Summary: React.FC<Props> = ({ vizEvent = undefined }) => {
         <div className={styles.summaryContent}>
           <h2 className="is-size-4">Ereignis Details</h2>
           <br></br>
-          {vizEvent ? (
+          {(vizEvent && vizEvent.title) ? (
             <>
               <p>Ereignis: {vizEvent.title}</p>
               <p>Datum: {vizEvent.date ? vizEvent.date : "unbekannt"}</p>
               <p>Quelle: {vizEvent.primSource}</p>
             </>
-          ) : null}
+          ) : (<>
+
+            <p>An dieser Stelle kann eine kurze Übrersicht über das gewählte Ereignis angezeigt werden. </p>
+            <br></br>
+            <p>Bitte <em>wählen</em> Sie dazu ein Ereignis auf der <em>Karte</em> oder in der <em>Leiste rechts</em>.</p>
+
+          </>)}
         </div>
       </div>
     </>
