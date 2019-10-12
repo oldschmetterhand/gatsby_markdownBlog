297aa090cb88fc2ad98a1ec19a5726fe4a045e73
Sebi <SebastianStoff@gmx.at>
2019-10-12 14:48:35 +0200

    changed title

--- src/components/specific/MapApp/VizController/AppLayout/AppTopBar/index.tsx
+++ src/components/specific/MapApp/VizController/AppLayout/AppTopBar/index.tsx
@@ -7,7 +7,7 @@ const AppTopBar = () => {
     return <div className={["columns is-gapless", styles.appTopBar].join(" ")}>
       <div className={["column is-3", styles.is3].join(" ")}>
         <h1 className="is-size-5">
-            Prosop |Viz
+            ProsopograAPhI |Viz
         </h1>
       </div>
       <div className={["column", styles.column].join(" ")}></div>
