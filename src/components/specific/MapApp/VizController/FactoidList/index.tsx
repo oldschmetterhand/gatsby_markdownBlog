0a0f1b74a7e697568644cadca4011afed910b89f
Sebi <SebastianStoff@gmx.at>
2019-10-12 20:29:48 +0200

    refined the list

--- src/components/specific/MapApp/VizController/FactoidList/index.tsx
+++ src/components/specific/MapApp/VizController/FactoidList/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { ProsopApiFactoid } from "../../index"
 
-const FactoidList = () => {
+interface props {
+    factoids?: ProsopApiFactoid[]
+}
+
+const FactoidList = ({factoids = undefined}) => {
     return <p>
         Factoid List
     </p>
