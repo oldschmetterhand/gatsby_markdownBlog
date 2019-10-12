3f1f975a56818dd81afe705137c25663ae0a47ef
Sebi <SebastianStoff@gmx.at>
2019-10-12 14:03:37 +0200

    added some styling for the search

--- src/components/specific/MapApp/VizController/SearchNView/Search/index.tsx
+++ src/components/specific/MapApp/VizController/SearchNView/Search/index.tsx
@@ -1,12 +1,20 @@
-
 import React from "react"
 import BulmaInput from "../../../../../common/BulmaInput"
 interface Props {
-    handleQueryBuilding?: (string: string) => void
+  handleQueryBuilding?: (string: string) => void
 }
 
-const Search:React.FC<Props> = ({handleQueryBuilding = undefined}) => {
-    return <BulmaInput getInpVal={handleQueryBuilding}></BulmaInput>
+const Search: React.FC<Props> = ({ handleQueryBuilding = undefined }) => {
+  return (
+    <>
+      <h2 className="is-size-6">Eine Person suchen: </h2>
+      <br></br>
+      <BulmaInput getInpVal={handleQueryBuilding} placeHolder="Personen-ID"></BulmaInput>
+      <br></br>
+      <hr></hr>
+      <button className="button is-primary">Search</button>
+    </>
+  )
 }
 
-export default Search;
+export default Search
