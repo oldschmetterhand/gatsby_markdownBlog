5b59274eccd1dc73676952e48c41d1d27509e014
Sebi <SebastianStoff@gmx.at>
2019-10-12 07:54:49 +0200

    activating tabs

--- src/components/specific/MapApp/VizController/Summary/index.tsx
+++ src/components/specific/MapApp/VizController/Summary/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { VizEvent } from "../.."
 import styles from "./styles.module.scss"
+import Tabs from "../../../../common/Tabs"
 
 interface Props {
   vizEvent: VizEvent
@@ -8,17 +9,22 @@ interface Props {
 
 const Summary: React.FC<Props> = ({ vizEvent = undefined }) => {
   return (
-    <div className={["container", styles.summaryContainer].join(" ")}>
-      <h2 className="is-size-4">Ereignis Details</h2>
-      <br></br>
-      {vizEvent ? (
-        <>
-          <p>Ereignis: {vizEvent.title}</p>
-          <p>Datum: {vizEvent.date ? vizEvent.date : 'unbekannt'}</p>
-          <p>Quelle: {vizEvent.primSource}</p>
-        </>
-      ) : null}
-    </div>
+    <>
+      <div className={["container", styles.summaryContainer].join(" ")}>
+        <Tabs tabs={["Search","Result Details"]}></Tabs>
+        <div className={styles.summaryContent}>
+          <h2 className="is-size-4">Ereignis Details</h2>
+          <br></br>
+          {vizEvent ? (
+            <>
+              <p>Ereignis: {vizEvent.title}</p>
+              <p>Datum: {vizEvent.date ? vizEvent.date : "unbekannt"}</p>
+              <p>Quelle: {vizEvent.primSource}</p>
+            </>
+          ) : null}
+        </div>
+      </div>
+    </>
   )
 }
 
