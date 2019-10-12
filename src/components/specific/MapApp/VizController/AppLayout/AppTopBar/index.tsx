84386e9e52506fc49cd8a606d260481ed7ba27d6
Sebi <SebastianStoff@gmx.at>
2019-10-12 10:29:20 +0200

    did some restyling

--- src/components/specific/MapApp/VizController/AppLayout/AppTopBar/index.tsx
+++ src/components/specific/MapApp/VizController/AppLayout/AppTopBar/index.tsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.scss"
 
 const AppTopBar = () => {
 
-    return <section className="hero is-small">
+    return <section className="hero">
     <div className={["hero-body", styles.appTopBar].join(" ")}>
       <div className="container">
         <h1 className="is-size-5">
