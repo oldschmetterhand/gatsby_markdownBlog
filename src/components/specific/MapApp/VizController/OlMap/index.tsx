016c10e132354f1007f6578190778c8c906188b5
Sebi <SebastianStoff@gmx.at>
2019-10-16 18:07:13 +0200

    drawing simple map with open layers

--- src/components/specific/MapApp/VizController/OlMap/index.tsx
+++ src/components/specific/MapApp/VizController/OlMap/index.tsx
@@ -1,6 +1,9 @@
-import React from "react"
+import React, {useRef, useState, useEffect} from "react"
 import { VizEvent } from "../../index"
-
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
 
 interface Props {
     vizEvents: VizEvent[]
@@ -8,7 +11,31 @@ interface Props {
 
 
 const OlMap: React.FC<Props> = () => {
-    return <div></div>
+
+    // ref used to pass in reference to div id="map" internally.
+    const olMapRef = useRef(undefined) 
+
+    useEffect(()=>{
+
+        new Map({
+            target: olMapRef.current,
+            layers: [
+              new TileLayer({
+                source: new OSM({
+                  url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                })
+              })
+            ],
+            view: new View({
+              center: [0, 0],
+              zoom: 2
+            })
+          });
+
+
+    },[])
+
+    return <div ref={olMapRef} id="map" className="map" style={{width:'100%', height:'400px'}}></div>
 }
 
 export default OlMap;
