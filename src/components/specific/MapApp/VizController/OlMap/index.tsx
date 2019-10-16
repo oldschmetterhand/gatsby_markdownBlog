c172a1252c7a7b229fcdb1a19ad36f4aaf84c32c
Sebi <SebastianStoff@gmx.at>
2019-10-16 17:47:51 +0200

    implemented olmap

---
+++ src/components/specific/MapApp/VizController/OlMap/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { VizEvent } from "../../index"
+
+
+interface Props {
+    vizEvents: VizEvent[]
+}
+
+
+const OlMap: React.FC<Props> = () => {
+    return <div></div>
+}
+
+export default OlMap;
