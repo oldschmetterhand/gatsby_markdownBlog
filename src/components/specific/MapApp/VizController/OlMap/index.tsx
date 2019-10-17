cfff9910438f1fc99325525867003721eb8bb9a6
Sebi <SebastianStoff@gmx.at>
2019-10-17 11:20:31 +0200

    just drawing one simple featue

--- src/components/specific/MapApp/VizController/OlMap/index.tsx
+++ src/components/specific/MapApp/VizController/OlMap/index.tsx
@@ -6,6 +6,12 @@ import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 import { fromLonLat } from 'ol/proj'
 
+//Classes for drawing Markers on the the ol map.
+import Point from "ol/geom/Point"
+import Feature from 'ol/Feature';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+
 interface Props {
     vizEvents: VizEvent[]
 }
@@ -13,20 +19,32 @@ interface Props {
 
 const OlMap: React.FC<Props> = () => {
 
-    const [olMap, setOlMap] = useState<undefined | any>(undefined)
+    const [olMap, setOlMap] = useState<undefined | Map>(undefined);
 
     // ref used to pass in reference to div id="map" internally.
     const olMapRef = useRef(undefined) 
 
     useEffect(()=>{
         if(olMap)return;
-        
+
         let map = new Map({
             target: olMapRef.current,
             layers: [
               new TileLayer({
                 source: new XYZ({
                   url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                })
+              }),
+              new VectorLayer({
+                source: new VectorSource({
+                  features:[
+                    new Feature({
+                      geometry: new Point(fromLonLat([4.35247, 50.84673])),
+                      //can add property as needed
+                      label:'Somewhere in Belgium'
+                      
+                    })
+                  ]
                 })
               })
             ],
