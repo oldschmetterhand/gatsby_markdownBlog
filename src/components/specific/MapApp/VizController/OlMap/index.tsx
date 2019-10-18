81bca2931e9381b1c9cf5b18123bd2a579e3a883
Sebi <SebastianStoff@gmx.at>
2019-10-18 17:39:06 +0200

    commented out offset of the marker

--- src/components/specific/MapApp/VizController/OlMap/index.tsx
+++ src/components/specific/MapApp/VizController/OlMap/index.tsx
@@ -120,7 +120,7 @@ const OlMap: React.FC<Props> = ({vizEvents = dummyData}) => {
         element: popupOverlay.current,  //ref assigning
         //autoPan: true,
         positioning:"top-left",
-        offset:[-24,10],
+        //offset:[-24,10],
         autoPanAnimation: {
           duration: 250
         }
