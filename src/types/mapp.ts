3ed135ec015a5e363bbb78a93fe1f49b040e9576
Sebi <SebastianStoff@gmx.at>
2019-10-26 10:41:53 +0200

    added temporarily the leaflet marker type

--- src/types/mapp.ts
+++ src/types/mapp.ts
@@ -1,11 +1,21 @@
 import Feature from 'ol/Feature';
 import { Factoid } from './prosogrAPhI'
 
+export interface LeafletMarker {
+  x: number
+  y: number
+  popUpContent: string
+  group?: string
+  boundTo?: VizEvent
+  lMarkerRef?: any
+}
 export interface VizEvent {
   title: string,
   category?: string,
   date?: string,
-  lonLat: number[],
+  lonLat?: number[],
+
+  lMarker?: LeafletMarker,  //linked leaflet marker
   feature?: Feature,    //linked open layers map
   geoJson?: any,
   factoid?: Factoid,    //linked factoid if necessary
